0edd9e171e8c2b7dc8543010bf61270ec3066dcb
ghigbie <gehigbie@gmail.com>
2018-03-18 18:57:54 -0600

    Add header component

--- src/components/NewsList/NewsList.js
+++ src/components/NewsList/NewsList.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './NewsList.css';
 
 const NewsList = () => {
     return(
         <div>
             News List Item
         </div>
-    )
+    );
 };
 
 export default NewsList;
