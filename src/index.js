839baeea4334d4702b9a88f66210bc8a6ead3d97
George Higbie <gehigbie@gmail.com>
2018-03-19 15:28:36 +0000

    Change data source

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './NewsFeedApp';
+import NewsFeedApp from './NewsFeedApp';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<NewsFeedApp />, document.getElementById('react-container'));
