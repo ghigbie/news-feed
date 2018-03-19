93794e7f969d414dee2013810dd10fff180dcc4d
George Higbie <gehigbie@gmail.com>
2018-03-19 15:41:41 +0000

    Refactor to incldue news item

--- src/components/NewsList/NewsList.js
+++ src/components/NewsList/NewsList.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import './NewsList.css';
 
+import NewsItem from './../NewsItem/NewsItem';
+
 const NewsList = (props) => {
-    const items = props.news.map((newsItem) => {
-        return(
-                <div key={newsItem.id}>
-                    <h3>{newsItem.title}</h3>
-                    <div>
-                        {newsItem.feed}
-                    </div>
-                </div>
-               );
-    });
+    const items = props.news.map((newsItem) => (<NewsItem key={newsItem.id} newsItem={newsItem} />));
     
     return(
         <div>
