0a89acb0322d3758bb31984eb1ba04a708c38dc5
George Higbie <gehigbie@gmail.com>
2018-04-03 18:56:43 +0000

    Use glamor to change style

--- src/components/NewsItem/NewsItem.js
+++ src/components/NewsItem/NewsItem.js
@@ -6,11 +6,18 @@ const NewsItem = ({newsItem}) => {
     let news_item = css({
         padding: '20px',
         boxSizing: 'border-box',
-        borderBottom: '1px solid grey'
+        borderBottom: '1px solid grey',
+        ':hover':{
+            background: '#c7c8c9'
+        }
     });
     
+    let item_grey = css({
+        background: '#e5e6e8'
+    })
+    
     return (
-        <div>
+        <div {...news_item} {...item_grey}>
             <h3>{newsItem.title}</h3>
             <div>
                 {newsItem.feed}
